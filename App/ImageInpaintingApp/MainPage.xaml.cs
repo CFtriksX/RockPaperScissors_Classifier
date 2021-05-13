9a363280ee7f6485ccdb03576e267e0782d39827
CFtriksX <paul.gelas@epitech.eu>
2021-05-13 13:28:39 +0200

    Add the Xamarin app with an Image picker Add a gitignore file

---
+++ App/ImageInpaintingApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace ImageInpaintingApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnPickerClickedAsync(object sender, EventArgs e)
+        {
+            try
+            {
+                var result = await MediaPicker.PickPhotoAsync(new MediaPickerOptions
+                {
+                    Title = "Select a photo"
+                });
+
+                var stream = await result.OpenReadAsync();
+
+                pickedImage.Source = ImageSource.FromStream(() => stream);
+
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+    }
+}
